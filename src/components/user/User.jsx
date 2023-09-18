4e986a9c39ea1023949242d270be1cce44a7d06d
illya-onyshchuk <llayonys@gmail.com>
2023-09-18 21:36:47 +0300

    add loader and json server

--- src/components/user/User.jsx
+++ src/components/user/User.jsx
@@ -1,18 +1,18 @@
 import { useSelector } from "react-redux";
 import { useActions } from "../../hooks/useActions";
+import Loader from '../UI/loader/Loader'
 
 const User = () => {
-  const {isLoading, error, user} = useSelector(state => state.user)
-  const acotion = useActions()
+  const {isLoading, error, user} = useSelector(state => state.user);
+  const { getUserById } = useActions();
 
-  console.log(acotion)
   return (
     <div>
-      <button onClick={() => {}}>
+      <button onClick={() => getUserById()}>
         Get user
       </button>
       {isLoading
-       ? <div>Loading...</div>
+       ? <Loader />
        : error ? <div>{error.message}</div> 
        : user?.name ? <h1>User: {user.name}</h1>
        : <h1>User Not Found</h1>
--- src/hooks/useActions.js
+++ src/hooks/useActions.js
@@ -14,4 +14,4 @@ export const useActions = () => {
 
   return useMemo(() => bindActionCreators(rootActions,
     dispatch), [dispatch])
-}   
+}
